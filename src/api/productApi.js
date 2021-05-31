872048bb2f023f4e43b65d415c4e8ad7371d00f1
Quang Huy <vuquanghuy978@gmail.com>
2021-05-31 08:35:42 +0700

    all

--- src/api/productApi.js
+++ src/api/productApi.js
@@ -5,14 +5,11 @@ const productApi = {
     // Transform _page to _start
     const newParams = { ...params };
     newParams._start = !params._page || params._page <= 1 ? 0 : (params._page - 1) * (params._limit || 50);
-
     // Remove un-needed key
     delete newParams._page;
-
     // Fetch product list + count
     const productList = await axiosClient.get('/products', { params: newParams });
     const count = await axiosClient.get('/products/count', { params: newParams });
-
     // Build response and return
     return {
       data: productList,
